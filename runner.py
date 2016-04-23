b65e16b9ac9ab3804a4ffba2c18e2d77a0ede79b
Vivek Nair <vivekaxl@gmail.com>
2016-04-23 19:13:50 -0400

    experiment set up

--- runner.py
+++ runner.py
@@ -1,12 +1,14 @@
 from Problems.RF.rf_runner import hpcc_rf
 from Algorithms.GridSearch import grid_search
+from Algorithms.Random import random_samples
+from Algorithms.LHS import lhs
 
 
 problems = [hpcc_rf]
-algorithms = [grid_search]
+algorithms = [grid_search, random_samples, lhs]
 
 # RF Parameters
-rf_parameters =[[100], [7], [1], [50], [True]]
+rf_parameters =[[100], [7], [1], [50], [True, False]]
 
 
 for algorithm in algorithms:
